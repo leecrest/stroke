539564839f38966105d51a4e679de6d7ba344fe2
leecrest <281042207@qq.com>
2017-08-29 20:07:23 +0800

    增加工程代码

---
+++ Assets/Framework/GameMgr.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public enum EnGameState
+{
+    Ready = 1,
+    Start,
+    Pause,
+    Over,
+}
+
+public class GameMgr : MgrBase
+{
+    public static GameMgr It;
+    void Awake() { It = this; }
+
+    public EnGameState State;
+    public Transform GameRoot;
+    public LineRenderer Line;
+
+    private List<NodeData> m_Nodes;
+    private int m_TmpIndex;     // drag点的序号
+    private Vector3 m_TmpPos;   // drag点的坐标
+
+    public override void Init()
+    {
+        base.Init();
+        m_Nodes = new List<NodeData>();
+        m_TmpIndex = -1;
+        m_TmpPos = Vector3.zero;
+        GameReady();
+    }
+
+    public override void UnInit()
+    {
+        base.UnInit();
+    }
+
+    #region 游戏流程控制
+    public void GameReady()
+    {
+        State = EnGameState.Ready;
+        UIMgr.It.OpenUI("UIHome");
+    }
+
+    public void GameStart()
+    {
+        State = EnGameState.Start;
+        m_TmpIndex = -1;
+        UIMgr.It.OpenUI("UIGame");
+        LoadMap(1);
+        InitLine();
+    }
+
+    public void GamePause()
+    {
+        State = EnGameState.Pause;
+    }
+
+    public void GameResume()
+    {
+        State = EnGameState.Start;
+    }
+
+    public void GameOver()
+    {
+        State = EnGameState.Over;
+    }
+
+
+    #endregion
+
+    void LoadMap(int idx)
+    {
+        MapConfig map = Config.MapCfg[idx - 1];
+        NodeData node;
+        Transform tf;
+        for (int i = 0; i < map.nodes.Length; i++)
+        {
+            node = new NodeData();
+            node.cfg = map.nodes[i];
+            node.obj = ResMgr.It.CreatePrefab(Config.NodePrefab[(int)node.cfg.type - 1]);
+            tf = node.obj.transform;
+            tf.parent = GameRoot;
+            tf.localPosition = new Vector3(node.cfg.x, node.cfg.y);
+            tf.localScale = new Vector3(2, 2);
+            m_Nodes.Add(node);
+        }
+    }
+
+    void InitLine()
+    {
+        NodeData node = m_Nodes[0];
+        Line.positionCount = 2;
+        Line.SetPosition(0, node.obj.transform.localPosition);
+        node = m_Nodes[m_Nodes.Count - 1];
+        Line.SetPosition(1, node.obj.transform.localPosition);
+    }
+
+    #region 线条操作
+
+
+    private void Update()
+    {
+        if (State != EnGameState.Start) return;
+        if (Input.GetMouseButton(0))
+        {
+            m_TmpPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            m_TmpPos.z = 0;
+            if (m_TmpIndex < 0)
+            {
+                // 开始drag，计算当前点在哪个线段中，找出插入点的位置
+                Vector3 cur, next;
+                m_TmpIndex = -1;
+                for (int i = 0; i < Line.positionCount-1; i++)
+                {
+                    cur = Line.GetPosition(i);
+                    next = Line.GetPosition(i + 1);
+                    if (PointInLine(m_TmpPos, cur, next))
+                    {
+                        m_TmpIndex = i+1;
+                        break;
+                    }
+                }
+                if (m_TmpIndex < 0) return;
+
+                // 移动idx后的点
+                Line.positionCount++;
+                for (int i = Line.positionCount-1; i > m_TmpIndex; i--)
+                {
+                    Line.SetPosition(i, Line.GetPosition(i-1));
+                }
+                Line.SetPosition(m_TmpIndex, m_TmpPos);
+            }
+            else
+            {
+                Line.SetPosition(m_TmpIndex, m_TmpPos);
+            }
+        }
+        else if (m_TmpIndex > 0)
+        {
+            for (int i = m_TmpIndex; i < Line.positionCount-1; i++)
+            {
+                Line.SetPosition(i, Line.GetPosition(i + 1));
+            }
+            Line.positionCount--;
+            m_TmpIndex = -1;
+        }
+    }
+
+    // 判定点是否在两个端点内
+    bool PointInLine(Vector3 point, Vector3 begin, Vector3 end)
+    {
+        return true;
+    }
+
+   
+
+    #endregion
+}
---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EnNodeType
+{
+    Begin = 1,      // 开始节点
+    Mid,            // 中间节点
+    End,            // 结束节点
+}
+
+public class NodeConfig
+{
+    public int idx;
+    public EnNodeType type;
+    public float x;
+    public float y;
+}
+
+public class MapConfig
+{
+    public int id;
+    public NodeConfig[] nodes;
+}
+
+public class NodeData
+{
+    public NodeConfig cfg;
+    public GameObject obj;
+}
+
+
+public class Config {
+    public static readonly string[] NodePrefab = {
+        "Prefab/node_begin",
+        "Prefab/node_mid",
+        "Prefab/node_end",
+    };
+
+    public static readonly MapConfig[] MapCfg = {
+        new MapConfig {id = 1, nodes = new NodeConfig[] {
+            new NodeConfig {idx = 0, type = EnNodeType.Begin, x = -1, y = -1 },
+            new NodeConfig {idx = 1, type = EnNodeType.Begin, x = -1, y = 1 },
+            new NodeConfig {idx = 2, type = EnNodeType.Begin, x = 1, y = -1 },
+            new NodeConfig {idx = 3, type = EnNodeType.Begin, x = 1, y = 1},
+        } },
+    };
+	
+}
